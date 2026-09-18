387ed36b942c45882df488b39ed4f4ae71c61483
matachecheche <49599299+matachecheche@users.noreply.github.com>
2026-09-17 22:13:52 -0400

    Update SecurityConfig.java

--- backend/src/main/java/com/uagrm/activos/security/SecurityConfig.java
+++ backend/src/main/java/com/uagrm/activos/security/SecurityConfig.java
@@ -53,7 +53,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200",
+    "https://activos-si2.onrender.com");
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
